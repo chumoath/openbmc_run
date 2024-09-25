fc5607824949866d333c60737e9874e65ffd1396
chumoath <chumoath.guo@gmail.com>
2024-09-25 22:03:02 +0800

    提交openbmc运行文件

---
+++ run_full.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# ln -s ../openbmc/build/evb-ast2600/tmp/deploy/images/evb-ast2600/obmc-phosphor-image-evb-ast2600.static.mtd obmc-phosphor-image-evb-ast2600.static.mtd
+
+./qemu-system-arm -m 1024 -M ast2600-evb -nographic -drive file=./core-image-full-cmdline-evb-ast2600.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=::2222-:22,hostfwd=::2443-:443,hostfwd=udp::2623-:623,hostname=qemu
