f520dc5c61d14c381ee6135dd61ea18528ae09d2
chumoath <chumoath.guo@gmail.com>
2024-10-31 22:45:46 +0800

    添加12345端口

--- run_romulus.sh
+++ run_romulus.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-./qemu-system-arm -m 256 -M romulus-bmc -nographic -drive file=./obmc-phosphor-image-romulus.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu
+./qemu-system-arm -m 256 -M romulus-bmc -nographic -drive file=./obmc-phosphor-image-romulus.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostfwd=:0.0.0.0:12345-:12345,hostname=qemu
